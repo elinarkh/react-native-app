d82d1c9aa6725116081c9f0cf30a0df5fd0173ef
Dinara <dinara@Dinaras-MBP-2.Dlink>
2019-12-09 02:44:31 +0600

    add map

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
-
-export default function LinksScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';// remove PROVIDER_GOOGLE import if not using Google Maps
+export default class LinksScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle} />
+      </View>
+    );
+  }
 }
-
-LinksScreen.navigationOptions = {
-  title: 'Links',
-};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
